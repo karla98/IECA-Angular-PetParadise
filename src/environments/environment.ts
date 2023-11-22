c9b23bfbc1c4e66f2dbc86daa033f40c5ead721f
karla_rios <riosdiazkm@gmail.com>
2023-11-21 20:52:58 -0600

    Recursos globales: logo, footer imagenes, variables de colores y environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  ASSET_URL: './assets/images',
+  SERVER_URL: 'http://localhost:3000/api'
 };
 
 /*
